4cb9496b98c531b5153bc8e07ffd55aa7325d5e0
wtyjoycee <42783113+wtyjoycee@users.noreply.github.com>
2020-06-14 23:19:47 -0400

    background changed

---
+++ src/app/contacts/userinfo.service.ts
@@ -0,0 +1,29 @@
+export class ServersService {
+    private servers = [];
+  
+    getServers() {
+      return this.servers;
+    }
+  
+    getServer(id: number) {
+      const server = this.servers.find(
+        (s) => {
+          return s.id === id;
+        }
+      );
+      return server;
+    }
+  
+    updateServer(id: number, serverInfo: {name: string, status: string}) {
+      const server = this.servers.find(
+        (s) => {
+          return s.id === id;
+        }
+      );
+      if (server) {
+        server.name = serverInfo.name;
+        server.status = serverInfo.status;
+      }
+    }
+  }
+  
