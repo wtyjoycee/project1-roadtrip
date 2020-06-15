4b73311488fde0eed8c948b6ed5deb914ff992d5
wtyjoycee <42783113+wtyjoycee@users.noreply.github.com>
2020-06-14 21:49:35 -0400

    changes

--- src/app/routing.ts
+++ src/app/routing.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { AboutComponent } from './about/about.component';
+import { ContactUSComponent } from './contacts/contact-us/contact-us.component';
+import { ContactInfoComponent } from './contacts/contact-info/contact-info.component';
+import { TeamInfoComponent } from './contacts/team-info/team-info.component';
 
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'contacts', component: ContactsComponent },
+  { path: 'contacts', component: ContactsComponent, children: [
+    { path: 'contact-us', component: ContactUSComponent },
+    { path: 'contact-info', component: ContactInfoComponent },
+    { path: 'team-info', component: TeamInfoComponent },
+  ]},
   { path: 'about', component: AboutComponent},
 ];
 
